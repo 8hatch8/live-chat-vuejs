0fdc05b53ee37bdb509b0f3e38380da7c9de8f2f
8hatch8 <yutaka.pg@gmail.com>
2021-12-29 13:57:21 +0900

    Create SignupForm.vue

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>アカウント新規登録</h2>
+  <form action="">
+    <input type="text" required placeholder="名前" v-model="name">
+    <input type="text" required placeholder="メールアドレス" v-model="email">
+    <input type="text" required placeholder="パスワード" v-model="password">
+    <input type="text" required placeholder="もう一度パスワードを入力" v-model="passwordConfirmation">
+    <button>登録する</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name:                 "",
+      email:                "",
+      password:             "",
+      passwordConfirmation: "",
+    }
+  }
+}
+</script>
