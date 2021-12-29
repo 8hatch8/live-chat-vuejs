e8e93fb96f999db3272163335a7e040fdc5e9b61
8hatch8 <yutaka.pg@gmail.com>
2021-12-29 10:18:52 +0900

    Update Welcome.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 // パスに応じて表示するコンポーネントを切り替える（SPA）
 import { createRouter, createWebHistory } from 'vue-router'
 // import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome'
 
-const routes = []
+const routes = [
+  {
+    path:       '/',
+    name:       'Welcome', // ページの名前
+    component:  Welcome // vueファイルの指定（上で定義）
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,1 +1,16 @@
-test
+<template>
+  <div class="container welcome">
+    <p>ようこそ!</p>
+  </div>
+</template>
+
+<script>
+// @はメソッド、:は変数dataを渡す、子はdataをpropsで受け取る
+</script>
+
+<style scoped>
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
