66296728b4e777fc90ac6c20c56455053c2365ab
8hatch8 <yutaka.pg@gmail.com>
2021-12-29 21:07:15 +0900

    Add Signup function

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,15 +1,23 @@
 <template>
   <h2>アカウント新規登録</h2>
-  <form action="">
+  <form @submit="signUp">
     <input type="text" required placeholder="名前" v-model="name">
-    <input type="text" required placeholder="メールアドレス" v-model="email">
-    <input type="text" required placeholder="パスワード" v-model="password">
-    <input type="text" required placeholder="もう一度パスワードを入力" v-model="passwordConfirmation">
+    <input type="email" required placeholder="メールアドレス" v-model="email">
+    <input type="password" required placeholder="パスワード" v-model="password">
+    <input type="password" required placeholder="もう一度パスワードを入力" v-model="passwordConfirmation">
     <button>登録する</button>
+
+    <ul>
+      <div v-for="error in errors">
+        <li class="error">{{ error }}</li>
+      </div>
+    </ul>
+
   </form>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data(){
     return{
@@ -17,6 +25,29 @@ export default {
       email:                "",
       password:             "",
       passwordConfirmation: "",
+      errors:               [],
+    }
+  },
+  methods:{
+    async signUp(){
+      this.errors = []
+      try{
+        const res = await axios.post('http://localhost:3000/auth', {
+          // パラメータ
+          name:                   this.name,
+          email:                  this.email,
+          password:               this.password,
+          password_confirmation:   this.passwordConfirmation
+          // railsではpassword_confirmation:として受け取る？
+        })
+        if(!res){
+          throw new Error('アカウントを登録できませんでした')
+        }
+        console.log({ res })
+        return res
+      } catch (error) {
+        this.errors = error.response.data.errors.full_messages
+      }
     }
   }
 }
